0fc3c254a957c956487fd30fc3a207d0a96a48db
yvanD12345 <78367362+yvanD12345@users.noreply.github.com>
2022-10-29 15:58:58 +0200

    ok

--- app/src/main/java/com/example/tp1/RenplirOffreStage.java
+++ app/src/main/java/com/example/tp1/RenplirOffreStage.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class RenplirOffreStage extends AppCompatActivity {
 
 
-    EditText companyName,email,Contact,poste,ville,adresse,telephone,postalCode;
+    EditText companyName,email,Contact,poste,ville,numeroDeRue,nomRue,telephone,url,postalCode;
     Button bouttonPoster;
 
     DBHelper Tp1bd;
@@ -27,7 +27,9 @@ public class RenplirOffreStage extends AppCompatActivity {
         Contact =  findViewById(R.id.Contact);
         poste = findViewById(R.id.poste);
         ville = findViewById(R.id.ville);
-        adresse = findViewById(R.id.adresse);
+        numeroDeRue = findViewById(R.id.numeroRue);
+        nomRue = findViewById(R.id.nomRue);
+        url = findViewById(R.id.url);
         telephone = findViewById(R.id.editTextPhone);
         postalCode = findViewById(R.id.postalCode);
         ville = findViewById(R.id.ville);
@@ -42,18 +44,59 @@ public class RenplirOffreStage extends AppCompatActivity {
                 String nomPosteEnString = poste.getText().toString();
 
                 String sVille= ville.getText().toString();
-                String sAdresse= adresse.getText().toString();
+                String sAdresse = concatenationAdresse();
+
+                String sUrl = checkUrl();
+
                 String sTelephonee= telephone.getText().toString();
                 String sPostalCode = postalCode.getText().toString();
+                if(sUrl.equals("") || sAdresse.equals("")) {
 
-                
-                Boolean insertionReussi = Tp1bd.insertoffer(companyNameEnString,emailEnString,contactEnString,nomPosteEnString,sVille,sPostalCode,sAdresse,sTelephonee);
-                if(insertionReussi == true){
-                    Toast.makeText(RenplirOffreStage.this,"une nouvelle offre de stage a été postulée", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RenplirOffreStage.this, "l'offre de stage n'a pas été postuléw", Toast.LENGTH_SHORT).show();
+                }else{
+                    Boolean insertionReussi = Tp1bd.insertoffer(companyNameEnString, emailEnString, contactEnString, nomPosteEnString, sVille, sPostalCode, sAdresse, sTelephonee,sUrl);
+                    if (insertionReussi == true) {
+                        Toast.makeText(RenplirOffreStage.this, "une nouvelle offre de stage a été postulée", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RenplirOffreStage.this, "l'offre de stage n'a pas été postuléw", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
-                else {
-                    Toast.makeText(RenplirOffreStage.this,"l'offre de stage n'a pas été postuléw", Toast.LENGTH_SHORT).show();
+            }
+            public String concatenationAdresse(){
+               String checkNumAdresse = extractNumber(numeroDeRue.getText().toString());
+               if(!checkNumAdresse.equals("")){
+                   String adresse = numeroDeRue.getText().toString()+" "+ nomRue.getText().toString()+" "+ville.getText().toString();
+               }
+              return checkNumAdresse;
+            }
+            public String extractNumber(final String str) {
+
+                if(str == null || str.isEmpty()) return "";
+
+                StringBuilder sb = new StringBuilder();
+                boolean found = false;
+                for(char c : str.toCharArray()){
+                    if(Character.isDigit(c)){
+                        sb.append(c);
+                        found = true;
+                    } else if(found){
+                        // If we already found a digit before and this char is not a digit, stop looping
+                        break;
+                    }
                 }
+
+                return sb.toString();
+            }
+            public String checkUrl(){
+             String sUrl =   url.getText().toString();
+             if(sUrl.contains("https://")){
+                 return sUrl;
+             }
+             else{
+                 return "";
+             }
+
             }
         });
     }
